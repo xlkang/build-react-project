7411658c9a4fef84587a70a6b45505facbd0a178
petrvs66 <xlkang@mail.ustc.edu.cn>
2019-08-19 19:41:09 +0800

    集成react-redux,redux-thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,10 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import combineReducers from './reducers.js';
 
-let store = createStore(combineReducers);
+// let store = createStore(combineReducers);
+
+/* 引入thunkMiddleware中间件 */
+let store = createStore(combineReducers, applyMiddleware(thunkMiddleware));
 
 export default store;
