dd490ee31a122abc5fdf49489d689b5aca537f5b
petrvs66 <xlkang@mail.ustc.edu.cn>
2019-09-08 15:31:03 +0800

    使用json-server替代静态mockjs方案

--- mock/mock.js
+++ mock/mock.js
@@ -1,8 +1,21 @@
-import Mock from 'mockjs';
+// import Mock from 'mockjs';
  
- let Random = Mock.Random;
+// let Random = Mock.Random;
  
- Mock.mock('/api/user', {
-	'name': '@cname',
-	'intro': '@word(20)'
- });
+// Mock.mock('/api/user', {
+// 'name': '@cname',
+// 'intro': '@word(20)'
+// });
+
+let Mock = require('mockjs');
+
+var Random = Mock.Random;
+
+module.exports = function () {
+	var data = {};
+	data.user = {
+		'name': Random.cname(),
+		'intro': Random.word(20)
+	};
+	return data;
+};
